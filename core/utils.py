7bad47059cf8828f03e0c97bfebb551a78d6e99c
Chenxi Deng <chenxideng1026@gmail.com>
2019-11-07 14:54:24 +0800

    for safe3.9

--- core/utils.py
+++ core/utils.py
@@ -74,6 +74,7 @@ def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_la
     random.seed(0)
     random.shuffle(colors)
     random.seed(None)
+    bbox_mess = ''
 
     for i, bbox in enumerate(bboxes):
         coor = np.array(bbox[:4], dtype=np.int32)
@@ -93,8 +94,7 @@ def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_la
             cv2.putText(image, bbox_mess, (c1[0], c1[1]-2), cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale, (0, 0, 0), bbox_thick//2, lineType=cv2.LINE_AA)
 
-    return image
-
+    return image, bbox_mess
 
 
 def bboxes_iou(boxes1, boxes2):
